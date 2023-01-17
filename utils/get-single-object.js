36009fdc2bd3e08f5a5b8e3f83afc042bc81e53a
verystack <59302430+verystack@users.noreply.github.com>
2023-01-18 04:20:00 +1100

    Add fallback

--- utils/get-single-object.js
+++ utils/get-single-object.js
@@ -2,5 +2,23 @@ const {getAlgoliaIndex} = require('./get-algolia-index');
 
 exports.getSingleObject = async ({ indexName, objectID, algoliaAppId, browseObjectsApiKey }) => {
     const index = getAlgoliaIndex(indexName, algoliaAppId, browseObjectsApiKey);
-    return index.getObject(objectID);
+    return new Promise((resolve, reject) => {
+        index.getObject(objectID).then(res => {
+            resolve(res)
+        }).catch(err => {
+            setTimeout(() => {
+                index.getObject(objectID).then(res => {
+                    resolve(res)
+                }).catch(err => {
+                    setTimeout(() => {
+                        index.getObject(objectID).then(res => {
+                            resolve(res)
+                        }).catch(err => {
+                            reject(err)
+                        })
+                    }, 3000 * Math.random())
+                })
+            }, 3000 * Math.random())
+        })
+    })
 };
